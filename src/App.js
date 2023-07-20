eb10326026dd08a1f2daa37c3a5f660bf31a881e
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-07-20 04:18:47 -0700

    jj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
-function App() {
+import {React,useState} from 'react'
+
+const App = () => {
+  const [name , setname]= useState({
+ 
+    firstname:"",
+    lastname:""
+  })
+  
+     
+     
   return (
-    <div className="App">
-    ffffffffffffffffffffffffffffffffff
+    <div className='App'> 
+    <input type='text' placeholder='Fname'value={name.firstname} onChange={(e)=>setname({...name, firstname: e.target.value})} />
+    <h1>firstname:{name.firstname}</h1> 
+
+    <input type='text' placeholder='Fname'value={name.lastname} onChange={(e)=>setname({...name , lastname: e.target.value})}/>
+    <h1>lastname:{name.lastname}</h1>
+    
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
